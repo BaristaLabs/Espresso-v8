0fe7c7a2683b8ae37bff1b32ce2dc7c8557d6d04
Oceanswave <sean@baristalabs.io>
2019-04-25 09:13:11 -0400

    Suppress git warnings

--- win-build-v8-2.ps1
+++ win-build-v8-2.ps1
@@ -6,17 +6,22 @@ param (
 
 # Set Environment Variables
 # Add depot tools to the path
-$env:Path = "$PSScriptRoot\depot_tools\;" + [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+$currentPath = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+if ($currentPath -match "^$PSScriptRoot\depot_tools\;") {
+    $env:Path = "$PSScriptRoot\depot_tools\;" + $currentPath
+}
 $env:DEPOT_TOOLS_WIN_TOOLCHAIN = 0
 $env:GYP_MSVS_VERSION=2019
 
 # Configure Git
-git config --global user.name "My Name"
-git config --global user.email "my-name@chromium.org"
-git config --global core.autocrlf false
-git config --global core.filemode false
-git config --global branch.autosetupmerge always
-git config --global branch.autosetuprebase always
+git config --local user.name "My Name"
+git config --local user.email "my-name@chromium.org"
+git config --local core.autocrlf false
+git config --local core.filemode false
+git config --local branch.autosetupmerge always
+git config --local branch.autosetuprebase always
+# turn the detached message off
+git config --local advice.detachedHead false
 
 $path = "$PSScriptRoot\v8"
 # Fixes fetch error "LookupError: unknown encoding: cp65001"
--- win-build-v8-3.ps1
+++ win-build-v8-3.ps1
@@ -6,7 +6,10 @@ param (
 
 # Set Environment Variables
 # Add depot tools to the path
-$env:Path = "$PSScriptRoot\depot_tools\;" + [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+$currentPath = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+if ($currentPath -match "^$PSScriptRoot\depot_tools\;") {
+    $env:Path = "$PSScriptRoot\depot_tools\;" + $currentPath
+}
 $env:DEPOT_TOOLS_WIN_TOOLCHAIN = 0
 $env:GYP_MSVS_VERSION=2019
 
