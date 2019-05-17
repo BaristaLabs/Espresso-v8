aab71fa150d2d30d9ac87b06647377598cff6d9e
Sean McLellan <sean@baristalabs.io>
2019-05-17 14:57:04 -0400

    Add ability to check different channels

--- scripts/checkReleaseStatus.ps1
+++ scripts/checkReleaseStatus.ps1
@@ -1,36 +1,35 @@
 # Determines of a V8 Build needs to occur
+param (
+    [string]$channel = (&{If([string]::IsNullOrWhiteSpace($env:V8_CHANNEL)) {"beta"} Else {$env:V8_CHANNEL}})
+)
+
+Write-Output "Using '$Channel' Channel."
+
 $v8VersionTableUrl = "https://omahaproxy.appspot.com/all?csv=1"
 $response = Invoke-WebRequest -Uri $v8VersionTableUrl -UseBasicParsing
 $csv = ConvertFrom-CSV $response.content
 
-function Split-VersionNumber {
-    Param([string]$VersionNumber)
+function Find-NugetPackageVersion {
+    param (
+        [string]$packageName,
+        [string]$versionNumber
+    )
 
-    $segments = $VersionNumber.Split('.')
-    
-    [hashtable]$result = @{}
-    $result.Add('Major', ($segments[0] -as [int]))
-    $result.Add('Minor', ($segments[1] -as [int]))
-    $result.Add('Patch', ($segments[2] -as [int]))
-    $result.Add('Build', ($segments[3] -as [int]))
-    return $result
+    try { 
+        $response = Invoke-WebRequest -Uri "https://www.nuget.org/packages/$packageName/" -UseBasicParsing
+    } catch { 
+        Write-Output "Unable to find nuget package version"
+        return $false
+    }
+
+    $packageName = $packageName.Replace(".", "\.")
+    $versionNumber = $versionNumber.Replace(".", "\.")
+
+    $options = [System.Text.RegularExpressions.RegexOptions] "Singleline, IgnoreCase"
+    [regex]$rx = [regex]::new("<a\s+href=""\/packages\/$packageName\/$versionNumber""\s+title=""$versionNumber"">.*?<\/a>", $options)
+    return !$rx.IsMatch($response.content)
 }
 
-function Compare-VersionNumber {
-    Param([string]$a, [string]$b)
-    $va = Split-VersionNumber -VersionNumber $a
-    $vb = Split-VersionNumber -VersionNumber $b
-
-    if ($va.Major -lt $vb.Major -or
-        $va.Minor -lt $vb.Minor -or 
-        $va.Patch -lt $vb.Patch -or 
-        $va.Build -lt $vb.Build) {
-            return $true
-        }
-    return $false
-}
-
-$channel = "beta"
 $win64Stable = $csv | Where-Object {$_.os -eq "win64" -and $_.channel -eq $channel} | Select-Object -First 1
 $macOSStable = $csv | Where-Object {$_.os -eq "mac" -and $_.channel -eq $channel} | Select-Object -First 1
 $linuxStable = $csv | Where-Object {$_.os -eq "linux" -and $_.channel -eq $channel} | Select-Object -First 1
@@ -50,74 +49,43 @@ $env:V8_VERSION_MACOS = $latestStableVersion_macOS
 $latestStableVersion_linux = $linuxStable.v8_version
 $env:V8_VERSION_UBUNTU = $latestStableVersion_linux
 
-#Determine if there's a newer windows version.
-try { 
-    $response = Invoke-WebRequest -Uri "https://www.nuget.org/packages/BaristaLabs.Espresso.v8.win-x64.release/" -UseBasicParsing
-    $options = [System.Text.RegularExpressions.RegexOptions]::Singleline
-    [regex]$rx = [regex]::new("<title>.*?BaristaLabs.Espresso.v8.win-x64.release\s([\d+\.]+).*?</title>", $options)
-    $publishedVersion = $rx.Match($response.content).Groups[1].Value
-
-    if ((Compare-VersionNumber -a $publishedVersion -b $latestStableVersion_win) -eq $true) {
-        Write-Output "Windows Build needed. Published: $publishedVersion, ${channel}: $latestStableVersion_win"
-        $env:build_windows = 'true'
-    } elseif(![string]::IsNullOrWhiteSpace($env:FORCE_WINDOWS)) {
-        Write-Output "Windows Build forced. Published: $publishedVersion, Forced: $env:FORCE_WINDOWS"
-        $env:V8_VERSION_WINDOWS = $env:FORCE_WINDOWS
-        $env:build_windows = 'true'
-    } else {
-        Write-Output "Windows Build not needed. Published: $publishedVersion, ${channel}: $latestStableVersion_win"
-        $env:build_windows = 'false'
-    }
-} catch { 
-    Write-Output "Unable to determine published Windows version"
+# Determine if there's a published windows version that corresponds to the current channel version
+if (Find-NugetPackageVersion -packageName "BaristaLabs.Espresso.v8.win-x64.release" -versionNumber $latestStableVersion_win) {
+    Write-Output "Windows Build needed for $latestStableVersion_win"
     $env:build_windows = 'true'
+} elseif(![string]::IsNullOrWhiteSpace($env:FORCE_WINDOWS)) {
+    Write-Output "Windows Build forced. Published: $publishedVersion, Forced: $env:FORCE_WINDOWS"
+    $env:V8_VERSION_WINDOWS = $env:FORCE_WINDOWS
+    $env:build_windows = 'true'
+} else {
+    Write-Output "Windows Build not needed for $latestStableVersion_win"
+    $env:build_windows = 'false'
 }
 
-#Determine if there's a newer macOS version.
-try { 
-    $response = Invoke-WebRequest -Uri "https://www.nuget.org/packages/BaristaLabs.Espresso.v8.macOS-x64.release/" -UseBasicParsing
-    $options = [System.Text.RegularExpressions.RegexOptions]::Singleline
-    [regex]$rx = [regex]::new("<title>.*?BaristaLabs.Espresso.v8.macOS-x64.release\s([\d+\.]+).*?</title>", $options)
-    $publishedVersion = $rx.Match($response.content).Groups[1].Value
-
-    if ((Compare-VersionNumber -a $publishedVersion -b $latestStableVersion_macOS) -eq $true) {
-        Write-Output "macOS Build needed. Published: $publishedVersion, ${channel}: $latestStableVersion_macOS"
-        
-        $env:build_macOS = 'true'
-    } elseif(![string]::IsNullOrWhiteSpace($env:FORCE_MACOS)) {
-        Write-Output "macOS Build forced. Published: $publishedVersion, Forced: $env:FORCE_MACOS"
-        $env:V8_VERSION_MACOS = $env:FORCE_MACOS
-        $env:build_macOS = 'true'
-    } else {
-        Write-Output "macOS Build not needed. Published: $publishedVersion, ${channel}: $latestStableVersion_macOS"
-        $env:build_macOS = 'false'
-    }
-} catch { 
-    Write-Output "Unable to determine published macOS version"
+# Determine if there's a published macOS version that corresponds to the current channel version
+if (Find-NugetPackageVersion -packageName "BaristaLabs.Espresso.v8.macOS-x64.release" -versionNumber $latestStableVersion_macOS) {
+    Write-Output "macOS Build needed for $latestStableVersion_macOS"
     $env:build_macOS = 'true'
+} elseif(![string]::IsNullOrWhiteSpace($env:FORCE_MACOS)) {
+    Write-Output "macOS Build forced. Published: $publishedVersion, Forced: $env:FORCE_MACOS"
+    $env:V8_VERSION_MACOS = $env:FORCE_MACOS
+    $env:build_macOS = 'true'
+} else {
+    Write-Output "macOS Build not needed for $latestStableVersion_macOS"
+    $env:build_macOS = 'false'
 }
 
 #Determine if there's a newer ubuntu version.
-try { 
-    $response = Invoke-WebRequest -Uri "https://www.nuget.org/packages/BaristaLabs.Espresso.v8.ubuntu-x64.release/" -UseBasicParsing
-    $options = [System.Text.RegularExpressions.RegexOptions]::Singleline
-    [regex]$rx = [regex]::new("<title>.*?BaristaLabs.Espresso.v8.ubuntu-x64.release\s([\d+\.]+).*?</title>", $options)
-    $publishedVersion = $rx.Match($response.content).Groups[1].Value
-
-    if ((Compare-VersionNumber -a $publishedVersion -b $latestStableVersion_linux) -eq $true) {
-        Write-Output "Ubuntu Build needed. Published: $publishedVersion, ${channel}: $latestStableVersion_linux"
-        $env:build_ubuntu = 'true'
-    } elseif(![string]::IsNullOrWhiteSpace($env:FORCE_UBUNTU)) {
-        Write-Output "Ubuntu Build forced. Published: $publishedVersion, Forced: $env:FORCE_UBUNTU"
-        $env:V8_VERSION_UBUNTU = $env:FORCE_UBUNTU
-        $env:build_ubuntu = 'true'
-    } else {
-        Write-Output "Ubuntu Build not needed. Published: $publishedVersion, ${channel}: $latestStableVersion_linux"
-        $env:build_ubuntu = 'false'
-    }
-} catch { 
-    Write-Output "Unable to determine Published Ubuntu version"
+if (Find-NugetPackageVersion -packageName "BaristaLabs.Espresso.v8.ubuntu-x64.release" -versionNumber $latestStableVersion_linux) {
+    Write-Output "Ubuntu Build needed for $latestStableVersion_linux"
     $env:build_ubuntu = 'true'
+} elseif(![string]::IsNullOrWhiteSpace($env:FORCE_UBUNTU)) {
+    Write-Output "Ubuntu Build forced. Published: $publishedVersion, Forced: $env:FORCE_UBUNTU"
+    $env:V8_VERSION_UBUNTU = $env:FORCE_UBUNTU
+    $env:build_ubuntu = 'true'
+} else {
+    Write-Output "Ubuntu Build not needed for $latestStableVersion_linux"
+    $env:build_ubuntu = 'false'
 }
 
 # set the multi-job variables
